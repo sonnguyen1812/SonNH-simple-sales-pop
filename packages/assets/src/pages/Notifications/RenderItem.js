d0eb8ebc5c5e87472f31656f3eb60503a175f628
Son Nguyen <son18122001@gmail.com>
2024-12-11 14:15:26 +0700

    Update RenderItem.js

--- packages/assets/src/pages/Notifications/RenderItem.js
+++ packages/assets/src/pages/Notifications/RenderItem.js
@@ -1,42 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import NotificationPopup from '@assets/components/NotificationPopup/NotificationPopup';
 import {ResourceItem} from '@shopify/polaris';
-import {api} from '../../helpers';
 import timestampToRelativeTime from '@assets/helpers/utils/timestampToRelativeTime';
-import useFetchApi from '@assets/hooks/api/useFetchApi';
 
 const renderItem = item => {
   const {firstName, city, productName, country, timestamp, productImage, id} = item;
   const time = timestampToRelativeTime(timestamp);
-  // const {data: notifications, setData: setNotifications} = useFetchApi({
-  //   url: '/notifications'
-  // });
-  // const [fetched, setFetched] = useState(false);
-  // const [loading, setLoading] = useState(false);
-
-  const handleDeleteNotification = async () => {
-    try {
-      setLoading(true);
-      const res = await api(`/notifications/${id}`, {
-        method: 'DELETE'
-      });
-      // setData([...notifications, setNotifications]);
-      // setLoading(false);
-      // setFetched(true);
-      if (!res.ok) {
-        throw new Error('Failed to delete notification');
-      }
-      console.log('Notification deleted:', id);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
 
   return (
     <ResourceItem id={id}>
       <NotificationPopup
-        onDeleteNotification={handleDeleteNotification}
         id={id}
         firstName={firstName}
         city={city}
